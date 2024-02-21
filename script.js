0ec7af9f2637f23dc5c85445d1e041067ad2989c
prasadleela <leelaprasad046@gmail.com>
2024-02-21 20:20:38 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function beforesubmit(){
+let outputdate=document.querySelector(".outputdate");
+let inputdate=document.querySelector(".inputdate");
+console.log("inputdate",inputdate.value);// string-->date(en_IN)
+let formatedDate=new Date(inputdate.value).toLocaleDateString("en-IN");
+outputdate.value=formatedDate;
+}
