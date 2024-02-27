32606b2257a92e3e1e97f29932feb9a2b6fa1974
prasadleela <leelaprasad046@gmail.com>
2024-02-27 23:11:23 +0530

    adding reCAPTCHA

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function beforesubmit(){
-let outputdate=document.querySelector(".outputdate");
-let inputdate=document.querySelector(".inputdate");
-console.log("inputdate",inputdate.value);// string-->date(en_IN)
-let formatedDate=new Date(inputdate.value).toLocaleDateString("en-IN");
-outputdate.value=formatedDate;
+function beforesubmit() {
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate", inputdate.value); // string-->date(en_IN)
+  let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+  outputdate.value = formatedDate;
 }
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
